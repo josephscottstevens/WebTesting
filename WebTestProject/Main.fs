ee4ebd48c8b8fc8bf75b461c0e2fefe971c8110c
Joseph Stevens <jstevens@uscarenet.com>
2017-10-12 15:11:27 -0400

    now cleans up existing user, added some todos, updated test case

--- WebTestProject/Main.fs
+++ WebTestProject/Main.fs
@@ -4,20 +4,43 @@ open FSharp.Data.Sql
 let [<Literal>] ConnectionString = "Data Source=localhost;Initial Catalog=NavcareDB_interface;Integrated Security=True; "
 type Sql = SqlDataProvider<ConnectionString = ConnectionString, DatabaseVendor = Common.DatabaseProviderTypes.MSSQLSERVER, UseOptionTypes = true>
 
+let newUserName = "TestUser@uscarenet.com"
 let newUserId = System.Guid.NewGuid().ToString()
 
-let SetupUser _ =
+
+// TODO: Switch over to hardCoded UserId, so we can more reliably delete test data
+let CleanupExistingUser =
     let ctx = Sql.GetDataContext()
-    let maybeUser = ctx.Dbo.AspNetUsers |> Seq.where (fun t-> t.UserName = "TestUser@uscarenet.com") |> Seq.tryHead
+    let maybeUser = ctx.Dbo.AspNetUsers |> Seq.where (fun t-> t.UserName = newUserName) |> Seq.tryHead
     if maybeUser.IsSome then 
         let usrId = maybeUser.Value.Id
         let maybePatient = ctx.Ptn.Patients |> Seq.where (fun t-> t.UserId = usrId) |> Seq.tryHead
         if maybePatient.IsSome then
+            let maybeTask = ctx.Tsk.Tasks |> Seq.where(fun t -> t.PatientId = maybePatient.Value.Id) |> Seq.tryHead
+            if maybeTask.IsSome then
+                maybeTask.Value.Delete()
+                ctx.SubmitUpdates()
+            let maybeProvider = ctx.Ptn.ProvidersPatientsMap |> Seq.where(fun t -> t.PatientId = Some maybePatient.Value.Id) |> Seq.tryHead
+            if maybeProvider.IsSome then
+                maybeProvider.Value.Delete()
+                ctx.SubmitUpdates()
+            let autoHistory = ctx.Tsk.AutomatedTasksTriggerHistory |> Seq.where(fun t -> t.PatientId = maybePatient.Value.Id) |> Seq.tryHead
+            if autoHistory.IsSome then
+                autoHistory.Value.Delete()
+                ctx.SubmitUpdates()
             maybePatient.Value.Delete()
+            ctx.SubmitUpdates()
         maybeUser.Value.Delete()
-    ctx.SubmitUpdates()
+        ctx.SubmitUpdates()
+    ignore
+
+// Note, need to disable trigger on ptn.Patient
+// Note, need to disable trigger on ptn.ProvidersPatientsMap
+let SetupUser _ =
+    CleanupExistingUser()
+    let ctx = Sql.GetDataContext()
     let usr = ctx.Dbo.AspNetUsers.``Create(AccessFailedCount, CanAddManualTime, EmailConfirmed, IsBillable, IsOnCall, LockoutEnabled, PhoneNumberConfirmed, RegistrationDate, TwoFactorEnabled, UserName)``
-                (0, true, true, true, true, false, true, System.DateTime.Now, false, "TestUser@uscarenet.com")
+                (0, true, true, true, true, false, true, System.DateTime.Now, false, newUserName)
     usr.Id <- newUserId
     usr.PasswordHash <- Some "AAgvTSlzShga1HVDEBJf76Whv2QfsihPA9xeM23UZozf975lVLk+ZcDljC5E1MDu6w=="
     usr.SecurityStamp <- Some "31fc6d9c-6a0a-4009-b93a-712218d6c130"
@@ -26,6 +49,12 @@ let SetupUser _ =
 
     ctx.Ptn.Patients.``Create(AccountNo, AutoSync, HasAccessToDashboard, HcoId, IsActive, IsCCM, IsEligibleForAWV, IsRemoved, OpenTasksCount, OpenTocsCount, PercentEnrollmentCompleted, UserId)``
                 (0, false, 0, 2, 0, true, true, false, 0, 0, 0, newUserId) |> ignore
+    ctx.SubmitUpdates()
+    let newPtn = ctx.Ptn.Patients |> Seq.where (fun t-> t.UserId = newUserId) |> Seq.head
+
+    let provider = ctx.Ptn.ProvidersPatientsMap.``Create(IsMainProvider)``(true)
+    provider.PatientId <- Some newPtn.Id
+    provider.ProviderId <- Some 2375
     ctx.SubmitUpdates()
     
     "User, Test"
--- WebTestProject/Program.fs
+++ WebTestProject/Program.fs
@@ -3,7 +3,7 @@ open Main
 
 let UserName = SetupUser()
 
-elementTimeout <- 30.0
+//elementTimeout <- 30.0
 chromeDir <- __SOURCE_DIRECTORY__
 start chrome
 
@@ -14,6 +14,8 @@ start chrome
     click "#SetPasswordSubmitImg"
     displayed "#mainNav"
 
+// It would be nice to test that validation is working as well
+// TODO: change Provider and Sex type to have Id's just like Language does
 "Verify Patient data" &&& fun _ ->
     click "a[href='/people']"
     "#patientsGridView_DXFREditorcol2_I" << UserName
@@ -21,8 +23,12 @@ start chrome
     click "#patientsGridView_tccell0_2 > a"
     click ".btn.btn-sm.btn-success.fa.fa-user"
     "#MiddleId" << "Test"
+    "#SSNId" << "000112222"
+    "#DateOfBirth" << "10/27/1988"
+    click "#MainProviderId"
+    click "#ejControl_3_popup > div > ul > li > span"
     click "#SexTypeId"                                         
-    click "#ejControl_5_popup > div > ul > li:nth-child(1)"
+    click "#ejControl_7_popup > div > ul > li:nth-child(1)"
     click "#LanguageId0_hidden"
     click "#LanguageId0_popup > div.e-content > ul > li:nth-child(2)"
     click "#SubmitId"
@@ -30,7 +36,7 @@ start chrome
     "#MiddleId" == "Test"
     "#SexTypeId" == "Male"
     "#LanguageId0_hidden" == "Afar"
-    
+    "#MainProviderId" == "Brown, Rush"
 run()
 
 printfn "press [enter] to exit"
