d35d62d38358adf2c98a31d331a17cc5e99aa0bc
Joseph Stevens <jstevens@uscarenet.com>
2017-10-12 12:14:12 -0400

    update

--- WebTestProject/Program.fs
+++ WebTestProject/Program.fs
@@ -7,14 +7,14 @@ start chrome
 
 SetupUser()
 
-"User Logins in, and sees the main navigation bar" &&&& fun _ ->
+"User Logins in, and sees the main navigation bar" &&& fun _ ->
     url "https://localhost:44336/"
     "#Email_I" << "jstevens@uscarenet.com"
     "#Password_I" << "History12!"
     click "#SetPasswordSubmitImg"
     displayed "#mainNav"
 
-"Verify Patient data" &&&& fun _ ->
+"Verify Patient data" &&& fun _ ->
     click "a[href='/people']"
     "#patientsGridView_DXFREditorcol2_I" << FullName
     press enter
@@ -23,15 +23,13 @@ SetupUser()
     "#MiddleId" << "Test"
     click "#SexTypeId"                                         
     click "#ejControl_5_popup > div > ul > li:nth-child(1)"
-    click "#SubmitId"
-    notDisplayed "#NotificationMessage"
     click "#LanguageId0_hidden"
-    click "#LanguageId0_popup > div.e-content > ul > li:nth-child(51)"
+    click "#LanguageId0_popup > div.e-content > ul > li:nth-child(2)"
     click "#SubmitId"
     reload()
     "#MiddleId" == "Test"
     "#SexTypeId" == "Male"
-    "#PatientLanguagesMap0_hidden" == "English"
+    "#LanguageId0_hidden" == "Afar"
     
 run()
 
