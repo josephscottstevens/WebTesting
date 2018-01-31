a1b9a286abfb25a4d35cd968982ab743042cdd88
josephscottstevens <josephscottstevens@gmail.com>
2018-01-31 15:48:08 -0500

    t

--- WebTestProject/Example.fs
+++ WebTestProject/Example.fs
@@ -5,7 +5,7 @@ open System
 open Main
 
 let runExample =
-    "Data is set properly" &&& fun _ ->
+    "Some description goes here, totally optional" &&& fun _ ->
         url "https://recruiting.ultipro.com/CAR1037/JobBoard/f0c16bb3-7879-3406-ff38-bfdd3e3afea8/OpportunityDetail?opportunityId=3d6bb0e8-80c4-41c8-aa47-45618a58b8cf"
         displayed "#Skills > div:nth-child(3) > div > div > div > div:nth-child(1) > div.col-xs-14.col-sm-12 > strong"
     
