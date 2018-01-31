bbfaf4625fbb23a92324c2e73c53a02b15497f0e
josephscottstevens <josephscottstevens@gmail.com>
2018-01-31 15:29:01 -0500

    t

--- WebTestProject/Program.fs
+++ WebTestProject/Program.fs
@@ -5,8 +5,8 @@ open SkillChecking
 chromeDir <- __SOURCE_DIRECTORY__
 start chrome
 
-runExample()
-//runSkillChecking()
+//runExample()
+runSkillChecking
 
 run()
 System.Console.ReadLine() |> ignore
--- WebTestProject/SkillChecking.fs
+++ WebTestProject/SkillChecking.fs
@@ -1,6 +1,7 @@
 ﻿module SkillChecking
 
 open canopy
+open System
 
 let runSkillChecking =
     let urls = 
@@ -9,10 +10,20 @@ let runSkillChecking =
         ; "https://recruiting.ultipro.com/CAR1037/JobBoard/f0c16bb3-7879-3406-ff38-bfdd3e3afea8/OpportunityDetail?opportunityId=3d6bb0e8-80c4-41c8-aa47-45618a58b8cf"
         ]
     let mutable i = 0
+    let mutable skills = []
 
     many urls.Length (fun _ ->
         url urls.[i]
-        displayed "#Skills > div:nth-child(3) > div > div > div > div:nth-child(1) > div.col-xs-14.col-sm-12 > strong"
-        let skills = read "#Skills > div:nth-child(3) > div > div > div > div:nth-child(1) > div.col-xs-14.col-sm-12 > strong"
+        let skill = 
+            try 
+                read "#Skills > div:nth-child(3) > div > div > div > div:nth-child(1) > div.col-xs-14.col-sm-12 > strong"
+            with _ ->
+                ""
         i <- i + 1
+        skills <- skill :: skills
+    )
+
+    lastly (fun _ ->
+        Console.WriteLine(skills)
+        ()
     )
