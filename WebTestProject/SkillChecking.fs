8a26def98ecdcd86ff8fe979d3cbdb29814aa9db
josephscottstevens <josephscottstevens@gmail.com>
2018-02-01 12:02:57 -0500

    Added second column

--- WebTestProject/SkillChecking.fs
+++ WebTestProject/SkillChecking.fs
@@ -17,16 +17,19 @@ let runSkillChecking =
         let skill = someElement "#Skills > div:nth-child(3) > div > div > div > div:nth-child(1) > div.col-xs-14.col-sm-12 > strong"
          
         if skill.IsSome && skill.Value.GetCssValue("Display") = "none" then
-            skills <- urls.[i].Column1 :: skills
+            skills <- (urls.[i].Column1, urls.[i].Column2) :: skills
         else if skill.IsNone then
-            skills <- urls.[i].Column1 :: skills
+            skills <- (urls.[i].Column1, urls.[i].Column2) :: skills
         else if skill.IsSome && skill.Value.GetCssValue("Display") <> "none" && skill.Value.Text <> "Home Health Aide, Personal Care Aide" then 
-            skills <- urls.[i].Column1 :: skills
+            skills <- (urls.[i].Column1, urls.[i].Column2) :: skills
         i <- i + 1
     )
 
     lastly (fun _ ->
-        let allRows = List.reduce(fun t y -> t + "\n" + y) skills
+        let allRows = 
+            skills
+            |> List.map (fun (t, y) -> t + "," + y)
+            |> List.reduce(fun t y -> t + "\n" + y) 
         
         File.WriteAllText("C:\Cognauto\Automation Output\Ultiprofiles\OpportunitiesOutput.csv", allRows)
     )
