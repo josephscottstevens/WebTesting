68bc7e88dba252d59d6c40bbb218ae7b7f87db77
pokwaro <pokwaro@5CG6285VLB>
2018-02-05 10:58:42 -0500

    Updating Url

--- WebTestProject/SkillChecking.fs
+++ WebTestProject/SkillChecking.fs
@@ -12,35 +12,52 @@ let runSkillChecking =
 
     let mutable i = 0
     let mutable skills = []
-
-    before (fun _ -> 
-        let ctx = SqlTest4.GetDataContext()
-        let newUrl = ctx.Dbo.HrOpportunityUrls.Create() 
-        newUrl.OpportunityNumber <- "Test"
-        newUrl.OpportunityUrl <- Some"Test"   //optional field
-        ctx.SubmitUpdates()
-
-    )
-
+    //urls.Length
     many urls.Length (fun _ ->
-        url urls.[i].Column2
+        let newUrl = urls.[i].Column2
+        url newUrl 
         let skill = someElement "#Skills > div:nth-child(3) > div > div > div > div:nth-child(1) > div.col-xs-14.col-sm-12 > strong"
          
         if skill.IsSome && skill.Value.GetCssValue("Display") = "none" then
-            skills <- (urls.[i].Column1, urls.[i].Column2) :: skills
+            skills <- (urls.[i].Column1, newUrl) :: skills
         else if skill.IsNone then
-            skills <- (urls.[i].Column1, urls.[i].Column2) :: skills
+            skills <- (urls.[i].Column1, newUrl) :: skills
         else if skill.IsSome && skill.Value.GetCssValue("Display") <> "none" && skill.Value.Text <> "Home Health Aide, Personal Care Aide" then 
-            skills <- (urls.[i].Column1, urls.[i].Column2) :: skills
+            skills <- (urls.[i].Column1, newUrl) :: skills
         i <- i + 1
     )
 
     lastly (fun _ ->
+        let ctx = SqlTest4.GetDataContext()
+
+        // Delete all rows in table
+        ctx.Dbo.HrOpportunityUrls
+        |> Seq.toList
+        |> List.map (fun t -> 
+            t.Delete()
+            ctx.SubmitUpdates()
+        )
+        |> ignore
+    
+            
+
+        // Insert new rows
+        skills
+        |> List.map (fun (t,y ) -> 
+            let newUrl = ctx.Dbo.HrOpportunityUrls.Create() 
+            newUrl.OpportunityNumber <- t
+            newUrl.OpportunityUrl <- Some y   //optional field
+            ctx.SubmitUpdates()
+        )
+        |> ignore
+        
         let allRows = 
             skills
             |> List.map (fun (t, y) -> t + "," + y)
             |> List.reduce(fun t y -> t + "\n" + y) 
+
         
+
         File.WriteAllText("C:\Cognauto\Automation Output\Ultiprofiles\OpportunitiesOutput.csv", allRows)
     )
     
