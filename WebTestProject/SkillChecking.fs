5ba98679725502725cbcf358d8f8e66acca7a663
pokwaro <pokwaro@5CG6285VLB>
2018-02-05 11:27:33 -0500

    New URL was updated and is working correctly

--- WebTestProject/SkillChecking.fs
+++ WebTestProject/SkillChecking.fs
@@ -14,7 +14,10 @@ let runSkillChecking =
     let mutable skills = []
     //urls.Length
     many urls.Length (fun _ ->
-        let newUrl = urls.[i].Column2
+        let newUrlBefore = urls.[i].Column2
+        let idx = newUrlBefore.IndexOf('?')
+        let newUrl = "https://recruiting.ultipro.com/CAR1037/JobBoard/f0c16bb3-7879-3406-ff38-bfdd3e3afea8/OpportunityDetail" + newUrlBefore.[idx..]
+         
         url newUrl 
         let skill = someElement "#Skills > div:nth-child(3) > div > div > div > div:nth-child(1) > div.col-xs-14.col-sm-12 > strong"
          
