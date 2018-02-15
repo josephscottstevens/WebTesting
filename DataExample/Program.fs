d8bdd6a1e6430d04052124a53f52722fc669edbb
josephscottstevens <josephscottstevens@gmail.com>
2018-02-15 13:31:30 -0500

    added a task example

--- DataExample/Program.fs
+++ DataExample/Program.fs
@@ -1,35 +1,46 @@
 ﻿open FSharp.Data.Sql
 open XPlot.GoogleCharts
 
-let [<Literal>] ConnectionString = "Data Source=navsql;Initial Catalog=NavcareDB_Test4;Integrated Security=True; "
+let [<Literal>] ConnectionString = "Data Source=localhost;Initial Catalog=NavcareDB_interface2;Integrated Security=True; "
 type Sql = SqlDataProvider<ConnectionString = ConnectionString, DatabaseVendor = Common.DatabaseProviderTypes.MSSQLSERVER, UseOptionTypes = true>
-
 let ctx = Sql.GetDataContext()
 
 [<EntryPoint>]
 let main _ = 
 
-
-    ctx.Ptn.PatientDemographics
-    |> Seq.where (fun t-> t.Race.IsSome)
-    |> Seq.map(fun t-> t.Race.Value)
+    let beginningOfMonth = new System.DateTime(2018, 1, 1)
+    ctx.Tsk.Tasks
+    |> Seq.map (fun t -> 
+                        match t.IsComplete with 
+                        | true ->
+                            match t.FinishTime with 
+                            | Some finishTime -> 
+                                if finishTime.Date >= beginningOfMonth then
+                                    "Completed"
+                                else
+                                    "NotRelevant"
+                            | None ->
+                                "ErrorData"
+                        | false ->
+                            if t.DueTime.Date < beginningOfMonth then
+                                "PastDue"
+                            else
+                                "Open"
+    )
+    |> Seq.where (fun t -> t <> "NotRelevant")
     |> Seq.countBy (fun t -> t)
-    |> Seq.sortByDescending snd
-    |> Seq.take 5 
-    |> Chart.Column
-    |> Chart.WithTitle "Most common Race\Ethnicity"
-    |> Chart.Show
-
-
-    ctx.Ptn.PatientDemographics
-    |> Seq.where (fun t-> t.MaritalStatus.IsSome)
-    |> Seq.map (fun t-> t.MaritalStatus.Value)
-    |> Seq.countBy (fun t-> t)
-    |> Seq.sortByDescending snd
-    |> Seq.take 5
+    |> Seq.toList
     |> Chart.Pie
-    |> Chart.WithTitle "Most common marital status"
     |> Chart.Show
 
+    //ctx.Ptn.PatientDemographics
+    //|> Seq.where (fun t-> t.Race.IsSome)
+    //|> Seq.map(fun t-> t.Race.Value)
+    //|> Seq.countBy (fun t -> t)
+    //|> Seq.sortByDescending snd
+    //|> Seq.take 5 
+    //|> Chart.Column
+    //|> Chart.WithTitle "Most common Race\Ethnicity"
+    //|> Chart.Show
 
     0 // done
