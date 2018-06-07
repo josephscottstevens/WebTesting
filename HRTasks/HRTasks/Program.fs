3535d4343732d6721f5cadb9c3a68bbf5cdb53d8
pokwaro <pokwaro@5CG6285VLB>
2018-06-07 09:40:27 -0400

    Updated due to website changes

---
+++ HRTasks/HRTasks/Program.fs
@@ -0,0 +1,13 @@
+﻿open canopy
+open canopy.runner.classic 
+open System 
+open System.IO
+open FSharp.Data
+open OpenQA.Selenium.Chrome 
+open canopy
+open canopy
+
+before (fun _ ->
+    classic.url "https://ew13.ultipro.com/Login.aspx"
+
+
---
+++ WebTestProject/PracticeFusionProject.fs
@@ -0,0 +1,13 @@
+﻿module PracticeFusionProject
+
+open canopy
+open System
+open System.IO
+open FSharp.Data
+
+let runPracticeFusionProject = 
+    let url = "https://static.practicefusion.com/apps/ehr/index.html?#/login"
+    let logIn = someElement "
+
+
+
